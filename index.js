a0e7dd53bc4e54342966542896903a2d0f8944d8
Hisashi Nakamura <hisashi@sunvisor.net>
2019-03-23 10:51:16 +0900

    Change output format to google calendar csv

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch({
         headless: true
     });
-    
+
     async function getMonthCalendar(year, month) {
         const page = await browser.newPage();
         await page.goto(`https://www.as-web.jp/calendar?cy=${year}&cm=${month}`);
@@ -29,7 +29,8 @@ const puppeteer = require('puppeteer');
                 day = await daysCont.jsonValue();
                 const d = ('00' + day).slice(-2);
                 const m = ('00' + month).slice(-2);
-                day = `${year}-${m}-${d}`
+                // day = `${year}-${m}-${d}`
+                day = `${m}/${d}/${year}`
             }
             const eventEls = await tds[1].$$('.event');
             let events = [];
@@ -49,13 +50,56 @@ const puppeteer = require('puppeteer');
         return records;
     }
 
+    function googleCalendarHeader() {
+        return [
+            'Subject',
+            'Start Date',
+            'Start Time',
+            'End Date',
+            'End Time',
+            'All Day Event',
+            'Description',
+            'Location',
+            'Private',
+        ];
+    }
+
+    function toGoogleCalendarCsv(data) {
+        let result = [];
+
+        result.push(googleCalendarHeader());
+        data.forEach(datum => {
+            datum.events.forEach(event => {
+                result.push([
+                    event,
+                    datum.day,
+                    '',
+                    datum.day,
+                    '',
+                    'True',
+                    'motor sports',
+                    '',
+                    'True'
+                ]);
+            });
+        });
+
+        return result;
+    }
+
     const year = 2019;
     let result = [];
-    for(let month = 1; month < 13; month++) {
+    for (let month = 1; month < 13; month++) {
         const cal = await getMonthCalendar(year, month);
         result = result.concat(cal);
     }
-    process.stdout.write(JSON.stringify(result));
+
+
+    //process.stdout.write(JSON.stringify(result));
+    const rows = toGoogleCalendarCsv(result);
+    rows.forEach(row => {
+        process.stdout.write(row + "\n");
+    });
 
     setTimeout(() => {
         browser.close()
